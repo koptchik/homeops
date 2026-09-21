a41ca5f3c48874b4a7f43dcea70fadfd15826fe4
koptchik <zhortchik@gmail.com>
2026-09-21 11:07:13 +0700

    fix/refactorin-routers

--- backend/app/routers/services.py
+++ backend/app/routers/services.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, HTTPException
 from backend.app.models import Service, ServiceCreate
 
-router = APIRouter()
-services = []
+router = APIRouter(
+    prefix="/services",
+    tags=["services"],
+)
+services: list[Service] = []
 next_id = 1
 
 
-@router.post("/services")
+@router.post("/")
 def create_service(service: ServiceCreate):
     global next_id
     new_service = Service(
@@ -19,11 +22,11 @@ def create_service(service: ServiceCreate):
     next_id += 1
     return new_service
 
-@router.get("/services")
+@router.get("/")
 def get_services():
     return services
 
-@router.get("/services/{service_id}")
+@router.get("/{service_id}")
 def get_service(service_id: int):
     for service in services:
         if service_id == service.id:
@@ -33,7 +36,7 @@ def get_service(service_id: int):
         detail="Service not found"
     )
 
-@router.delete("/services/{service_id}")
+@router.delete("/{service_id}")
 def delete_service(service_id: int):
     for service in services:
         if service_id == service.id:
