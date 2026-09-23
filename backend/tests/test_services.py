0447d21fc31462dafa052348bfef3e8c0b55ff7a
Zhortchik <zhortchik@gmail.com>
2026-09-23 16:56:27 +0700

    test: add validation tests

--- backend/tests/test_services.py
+++ backend/tests/test_services.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from backend.app.main import app
+from pydantic import BaseModel,HttpUrl,ValidationError
 
 
 client = TestClient(app)
@@ -75,11 +76,63 @@ def test_delete_service():
     assert response.json() == created
 
 def test_get_service_999999():
-    response = client.get(f"/services/999999")
+    response = client.get("/services/999999")
 
     assert response.status_code == 404
 
 def test_delete_service_999999():
-    response = client.delete(f"/services/999999")
+    response = client.delete("/services/999999")
 
     assert response.status_code == 404
+
+def test_service_name_lenht():
+    lenghts = [1,2,50,51]
+    for lenght in lenghts:
+        original_string = "test"
+        random_chars = str(abs(hash(original_string))) * 4
+        result = random_chars[:lenght]
+
+        response = client.post(
+            "/services",
+            json={
+                "name": result,
+                "url": "http://exemple.com:1234",
+                "check_interval": 60
+            }
+        )
+        if lenght in range(2,50):
+            assert response.status_code == 200
+        else:
+            assert response.status_code == 422
+
+def test_service_check_interval():
+    intervals = [9,10,3600,3601]
+    for interval in intervals:
+        response = client.post(
+            "/services",
+            json={
+                "name": "test",
+                "url": "http://exemple.com:1234",
+                "check_interval": interval
+            }
+        )
+        if interval in range(10,3600):
+            assert response.status_code == 200
+        else:
+            assert response.status_code == 422
+
+def test_service_url():
+    urls = ["http://exemple.com:1234","exemple com"]
+    for url in urls:
+        response = client.post(
+            "/services",
+            json={
+                "name": "test",
+                "url": url,
+                "check_interval": 60
+            }
+        )
+        if url is HttpUrl:
+            assert response.status_code == 200
+        else:
+            assert response.status_code == 422
