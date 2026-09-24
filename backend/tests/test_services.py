cbd26bed78fdd7f3f2bfaaf23f7f40edd387f5d7
Zhortchik <zhortchik@gmail.com>
2026-09-24 11:50:45 +0700

    test: add test lifecycle service

--- backend/tests/test_services.py
+++ backend/tests/test_services.py
@@ -152,3 +152,29 @@ def test_service_url():
             }
         )
         assert response.status_code == expected_status
+
+def test_service_lifecycle():
+    created = client.post(
+        "/services",
+        json={
+            "name": "test",
+            "url": "http://exemple.com:1234"
+        }
+    )
+    created_id = created.json()["id"]
+    assert created.status_code == 200
+    assert created.json()["name"] == "test"
+    assert created.json()["url"] == "http://exemple.com:1234"
+    geted = client.get(f"/services/{created_id}")
+    assert geted.status_code == 200
+    assert geted.json()["name"] == "test"
+    assert geted.json()["url"] == "http://exemple.com:1234"
+    deleted = client.delete(f"/services/{created_id}")
+    assert deleted.status_code == 200
+    assert deleted.json() == created.json()
+    not_found = client.get(f"/services/{created_id}")
+    assert not_found.status_code == 404
+    checked_delete = client.get("/sevices")
+    assert created_id not in [item["id"] for item in checked_delete["body"]]
+
+        
